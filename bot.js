e25fe3e99eb5a809626f44c6fca46980f8b7c1f6
katabump8000-beep <katabump8000@gmail.com>
2026-09-12 21:03:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ const {
     useMultiFileAuthState,
     DisconnectReason,
     fetchLatestBaileysVersion
-} = require("baileys-pro");
+} = require("@whiskeysockets/baileys");
 
 const fs = require("fs");
 const path = require("path");
