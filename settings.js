7584494b62c0cb46e09727a512dc99013092e4ee
katabump8000-beep <katabump8000@gmail.com>
2026-09-25 19:01:09 +0300

    Update bot number and owner in settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 // settings.js - إعدادات البوت الأساسية
 
 const settings = {
-    botNumber: "201271931765", // ضع رقمك هنا
-    owners: ["201271931765"],
+    botNumber: "48699554086", // ضع رقمك هنا
+    owners: ["48699554086"],
     sessionFolder: "session",
     botName: "BOT PATHIRA"
 };
